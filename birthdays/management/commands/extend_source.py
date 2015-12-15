b1446d1f6169187653f00f199463b984a2692dfe
fako <email@fakoberkers.nl>
2015-12-15 21:17:06 +0100

    Remove obsolete methods of extend command.

--- birthdays/management/commands/extend_source.py
+++ birthdays/management/commands/extend_source.py
@@ -52,33 +52,6 @@ class Command(BaseCommand):
             person_source.master = master
             person_source.save()
 
-    @staticmethod
-    def lower_case_city(source_model):
-        for source in source_model.objects.filter(props__has_key="city"):
-            city = source.props["city"]
-            if city is not None and city.isupper():
-                source.props["city"] = source.props["city"].lower().capitalize()
-                source.save()
-
-    @staticmethod
-    def remove_immediate_duplicates(source_model):
-        previous = None
-        for person in source_model.objects.all():
-            if previous is None:
-                previous = person
-                continue
-            if person.full_name == previous.full_name and person.birth_date == previous.birth_date:
-                print("Deleting one:", person.full_name)
-                person.delete()
-            previous = person
-
-    @staticmethod
-    def remove_duplicates(source_model):
-        for person in source_model.objects.all():
-            if source_model.objects.filter(full_name=person.full_name, birth_date=person.birth_date).count() > 1:
-                print("Deleting one:", person.full_name)
-                person.delete()
-
     def add_arguments(self, parser):
         parser.add_argument(
             'extend_type',
