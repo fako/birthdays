f2376a452429391e0240530f63044c7b858e75a3
fako <email@fakoberkers.nl>
2015-12-21 07:36:13 +0100

    Fix Soccer split_full_name.

--- birthdays/models/sources/sports.py
+++ birthdays/models/sources/sports.py
@@ -3,8 +3,8 @@ from ..person import PersonSource
 
 class SoccerSource(PersonSource):
 
-    def split_full_name(self):
-        super(SoccerSource, self).split_full_name()
+    def split_full_name(self, force=False):
+        super(SoccerSource, self).split_full_name(force=force)
         self.initials = self.first_name if self.first_name is not None and len(self.first_name) <= 20 else None
         self.first_name = None
 
